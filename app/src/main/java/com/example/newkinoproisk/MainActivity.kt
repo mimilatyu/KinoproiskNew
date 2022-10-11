2824a1486125532487ece3e1341944c00cfcc3e7
mimilatyu <turmyshov@mail.ru>
2022-10-11 18:53:03 +0300

    added poster and buttons

--- app/src/main/java/com/example/newkinoproisk/MainActivity.kt
+++ app/src/main/java/com/example/newkinoproisk/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.newkinoproisk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.newkinoproisk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.buttonMenu.setOnClickListener {
+            Toast.makeText(this, "MENU", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonFav.setOnClickListener {
+            Toast.makeText(this, "FAVORITES", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonLater.setOnClickListener {
+            Toast.makeText(this, "LATER", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonCollect.setOnClickListener {
+            Toast.makeText(this, "COLLECTIONS", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonSet.setOnClickListener {
+            Toast.makeText(this, "SETTINGS", Toast.LENGTH_SHORT).show()
+        }
     }
 }
